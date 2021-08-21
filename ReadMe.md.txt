1f3d9e0a345400c01119c35a1dc9f5a26207f88b
unknown <kenzamer2017@gmail.com>
2021-08-20 22:34:36 -0400

    alternate between players

---
+++ ReadMe.md.txt
@@ -0,0 +1,6 @@
+_ Create 4 files: Style.css index.html app.js(main) connect4.js(class)
+_ the app.js will be the main js that calls the connect4 class
+_ inside the connect4.js create a class with constructor that have Rows and colums as attributes 
+_ create a method (creatgrid) inside the class connect 4 and Use Jquery and loops to create the rows
+ and colums on the html web page
+_ create eventlistner method to 
--- connect4.js
+++ connect4.js
@@ -3,8 +3,11 @@ class Connect4 {
     constructor(selector){
         this.ROWS=6;
         this.COLS =7;
+        this.player = 'blue'
         this.selector = selector;
         this.creatgrid();
+        this.eventListener();
+        
     }
 
     creatgrid(){
@@ -17,6 +20,9 @@ class Connect4 {
             for (let col =0 ; col <this.COLS; col++){
                 const $col = $('<div>')
                 .addClass('col empty')
+                //add index to the cellule
+                .attr("data-col", col)
+                .attr("data-row",row)
                 $row.append($col)
 
             } 
@@ -24,6 +30,53 @@ class Connect4 {
             $board.append($row)
         }
         
+    }
+    eventListener(){
+        // indicator for the place when the user hover over the cell
+        const $board = $(this.selector)
+        // set up a varibale that will take the value of this
+        const thisAlt = this
+        function findLastEmptyCell(col){
+            const cell = $(`.col[data-col='${col}']`)
+            //console.log(cell)
+            for (let i = cell.length ; i>0; i--) {
+                const $cell = $(cell[i])
+                if($cell.hasClass('empty')){
+                    return $cell;
+                }
+            }
+            return null
+
+        }
+        $board.on('mouseenter','.col.empty', function(){
+            console.log(('here',this));
+            const col= $(this).data('col')
+            const $lastEmptyCell = findLastEmptyCell(col)
+            $lastEmptyCell.addClass('next-${thisAlt.player}')
+            //console.log(col)
+        })
+        //remove the previouse cells that was hovered 
+        $board.on('mouseleave','.col',function(){
+            $('.col').removeClass('next-${thisAlt.player}')
+        })
+        // add an event click that allow the click on on cell and it will become colored with blue color
+        $board.on('click','.col.empty', function(){
+            const col= $(this).data('col')
+            //const row= $(this).data('row')
+            const $lastEmptyCell = findLastEmptyCell(col)
+            $lastEmptyCell.removeClass('empty next-${thisAlt.player}')
+            $lastEmptyCell.addClass(thisAlt.player)
+            // alternate between the two players
+            thisAlt.player = (thisAlt.player === 'blue') ? 'red' : 'blue'
+            console.log(thisAlt.player + ' it is your turn to play')
+
+            $(this).trigger('mouseenter')
+            
+        })
+        
+
+            
+
     }
 
 
